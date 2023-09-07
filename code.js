809c74beaf839140e62127014a88f2bb1fd1882d
Joel Chambers <104284414+Boomboomyoyo@users.noreply.github.com>
2023-09-07 09:37:59 -0600

    First changes (I think)

--- code.js
+++ code.js
@@ -1,7 +1,12 @@
+a = [1,2,3,4,5,6,7];
+
 function sum(a) {
-    var sum = a[0];
+    var sum = 0;
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
 }
+
+b = sum(a);
+console.log(b);
